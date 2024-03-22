736548278f1d15cbebdc521c054bd6336b5d528e
Sathish-kumar-asktech <sathish.asktech@gmail.com>
2024-03-22 11:43:52 +0530

    api

--- pages/api/CityRoute.js
+++ pages/api/CityRoute.js
@@ -1,34 +1,34 @@
-const express = require("express");
-const CityController = require("../../TechveelEnq_Controller/MasterController/CityController");
+// const express = require("express");
+// const CityController = require("../../TechveelEnq_Controller/MasterController/CityController");
 
-const router = express.Router();
-const jwt = require("jsonwebtoken");
-function verifytoken(req, res, next) {
-  const beareHeader = req.headers["authorization"];
-  if (typeof beareHeader != "undefined") {
-    const bearer = beareHeader.split(" ");
-    const bearerToken = bearer[1];
-    req.token = bearerToken;
-    jwt.verify(bearerToken, "secretkey", (err, user) => {
-      if (err) {
-        res.json({ error: "unauthorzhied user" });
-      } else {
-        next();
-      }
-    });
-  } else {
-    res.json({
-      error: "Unauthorized user",
-    });
-  }
-}
+// const router = express.Router();
+// const jwt = require("jsonwebtoken");
+// function verifytoken(req, res, next) {
+//   const beareHeader = req.headers["authorization"];
+//   if (typeof beareHeader != "undefined") {
+//     const bearer = beareHeader.split(" ");
+//     const bearerToken = bearer[1];
+//     req.token = bearerToken;
+//     jwt.verify(bearerToken, "secretkey", (err, user) => {
+//       if (err) {
+//         res.json({ error: "unauthorzhied user" });
+//       } else {
+//         next();
+//       }
+//     });
+//   } else {
+//     res.json({
+//       error: "Unauthorized user",
+//     });
+//   }
+// }
 
-router.get("/GetAllCity", CityController.GetAllCity);
-router.get("/GetOneCity/:id", verifytoken, CityController.GetOneCity);
-router.post("/InsertCity", verifytoken, CityController.InsertCity);
-router.put("/UpdateCity/:id", verifytoken, CityController.UpdateCity);
-router.delete("/DeleteCity/:id", verifytoken, CityController.DeleteCity);
+// router.get("/GetAllCity", CityController.GetAllCity);
+// router.get("/GetOneCity/:id", verifytoken, CityController.GetOneCity);
+// router.post("/InsertCity", verifytoken, CityController.InsertCity);
+// router.put("/UpdateCity/:id", verifytoken, CityController.UpdateCity);
+// router.delete("/DeleteCity/:id", verifytoken, CityController.DeleteCity);
 
-module.exports = {
-  TechveelEnq_Route: router,
-};
+// module.exports = {
+//   TechveelEnq_Route: router,
+// };
